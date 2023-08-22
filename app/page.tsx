ea1303b7fb025e41ebb945586710639161fb0266
Adrien KISSIE <fredkiss3@gmail.com>
2023-08-22 14:25:06 +0200

    ⬆️ upgrade next to canary to fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { revalidatePath } from "next/cache";
 import Link from "next/link";
+import { SubmitButton } from "./button";
 
 function wait(ms: number): Promise<void> {
   // Wait for the specified amount of time
@@ -9,15 +10,15 @@ function wait(ms: number): Promise<void> {
 export default function Home() {
   async function action() {
     "use server";
-    await wait(1000).then((_) => revalidatePath("/"));
+    await wait(5_000);
+    revalidatePath("/");
   }
   return (
     <main>
       <Link href={`/other-page`}>Go to other page</Link>
 
-      <h1>Bug with server actions blocking UI</h1>
       <form action={action}>
-        <button>Perform action</button>
+        <SubmitButton />
       </form>
     </main>
   );
